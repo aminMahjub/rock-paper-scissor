2c3f98c9eb254400488410ae18189243d51a48d7
Mmmad Amin Mahjub Asl <aminmahjoob3172@gmail.com>
2022-12-15 19:28:28 +0330

    Add module system

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 const usernames = document.querySelectorAll('.get-username-modal input[type="text"]');
 const savePlayerUsernameBtn = document.querySelector('.save-username');
 const modal = document.querySelector('.get-username-modal');
+export const gamePoint = document.querySelector('#limitPoint');
 
 savePlayerUsernameBtn.addEventListener('click', () => {
     openModal();
@@ -9,12 +10,14 @@ savePlayerUsernameBtn.addEventListener('click', () => {
 function openModal() {
     const usernamesPlace = document.querySelectorAll('.username');
    
-    if (usernames[0].value === '' && usernames[1].value == '') {
+    if (usernames[0].value === '' && usernames[1].value === '' && gamePoint.value === '') {
         alert('Inputs are empty');
     } else {
+
         if (usernames[0].value === usernames[1].value) {
             alert('Two input values are same please choose another name')
-        } else {
+        } else if (gamePoint.value == 0) alert('Game point has not to be zero');
+        else {
             [usernamesPlace[0].textContent, usernamesPlace[1].textContent] = [usernames[0].value, usernames[1].value];
             modal.classList.add('close-modal');
         }
